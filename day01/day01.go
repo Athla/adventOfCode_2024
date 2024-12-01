f3d709729773888c8cbdf4e6bebbdfa239459e95
Guilherme Rodrigues <guilher.c.rodrigues@gmail.com>
2024-12-01 19:56:33 -0300

    feat. complete day 01?

--- day01/day01.go
+++ day01/day01.go
@@ -14,36 +14,6 @@ type Input struct {
 	right uint
 }
 
-func QuickSort(arr []int) []int {
-	return quickSort(arr, 0, len(arr)-1)
-}
-
-func quickSort(arr []int, low, high int) []int {
-	if low < high {
-		arr, p := partition(arr, low, high)
-		arr = quickSort(arr, 0, p-1)
-		arr = quickSort(arr, p+1, high)
-	}
-
-	return arr
-}
-
-func partition(arr []int, low, high int) ([]int, int) {
-	pivot := arr[high]
-	i := low
-
-	for j := low; j < high; j++ {
-		if arr[j] < pivot {
-			arr[i], arr[j] = arr[j], arr[i]
-			i++
-		}
-	}
-
-	arr[i], arr[high] = arr[high], arr[i]
-
-	return arr, i
-}
-
 func doDiff(a, b int) int {
 	if a > b {
 		return (a - b)
@@ -53,20 +23,7 @@ func doDiff(a, b int) int {
 		return 0
 	}
 }
-func main() {
-	// Load file
-	f, err := os.Open("day01.csv")
-	if err != nil {
-		log.Fatalf("Unable to read file due: %v", err)
-	}
-	defer f.Close()
-	// Parse it into left and right sides
-	csvReader := csv.NewReader(f)
-	data, err := csvReader.ReadAll()
-	if err != nil {
-		log.Fatalf("Unable to read file due: %v", err)
-	}
-	// Sort it
+func FirstHalf(data [][]string) int {
 	var left []int
 	var right []int
 	for i := 0; i < len(data); i++ {
@@ -82,18 +39,59 @@ func main() {
 		}
 		right = append(right, vRight)
 	}
-	// Sort the two slices
 	sort.Ints(left)
 	sort.Ints(right)
-	log.Infof("Left size: %v Right size: %v", len(left), len(right))
-	// Do diff
+
 	diff := 0
 	for i := 0; i < 1000; i++ {
 		diff += doDiff(left[i], right[i])
 	}
 
 	log.Info(diff)
+	return diff
+}
+func main() {
+	f, err := os.Open("day01.csv")
+	if err != nil {
+		log.Fatalf("Unable to read file due: %v", err)
+	}
+	defer f.Close()
 
-	// Sum diff
+	csvReader := csv.NewReader(f)
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatalf("Unable to read file due: %v", err)
+	}
 
+	// Simlarity score
+	// Figure how many times the left side appears in the right side
+	// Multiply the number in the left side by the amount of times it appears in the right
+	// map left:numApp in the right side, if the key already exists jsut skip for now
+	var left []int
+	var right []int
+	for i := 0; i < len(data); i++ {
+		vLeft, err := strconv.Atoi(data[i][0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		left = append(left, vLeft)
+
+		vRight, err := strconv.Atoi(data[i][1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		right = append(right, vRight)
+	}
+	rightFreq := make(map[int]int)
+	for _, num := range right {
+		rightFreq[num]++
+	}
+
+	totalScore := 0
+	for _, num := range left {
+		totalScore += num * rightFreq[num]
+	}
+
+	log.Info(totalScore)
+	// Check how many time s the left number appears in the right side
 }
